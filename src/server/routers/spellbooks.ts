05fa953f4a8c000afddd75caea5874e07ce5c577
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-25 21:52:56 +0100

    feat: add spellbook and spell routers

---
+++ src/server/routers/spellbooks.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const spellBookRouter = router({
+    get: publicProcedure.query(async () => {
+        return prisma.spellbook.findMany();
+    }),
+    create: publicProcedure
+        .input(
+            z.object({
+                title: z.string(),
+                description: z.string(),
+            })
+        )
+        .mutation(async (opts) => {
+            const { input } = opts;
+            return prisma.spellbook.create({
+                data: {
+                    title: input.title,
+                    description: input.description,
+                },
+            });
+        }),
+    delete: publicProcedure
+        .input(
+            z.object({
+                id: z.number(),
+            })
+        )
+        .mutation(async (opts) => {
+            const { input } = opts;
+            return prisma.spellbook.delete({
+                where: {
+                    id: input.id,
+                },
+            });
+        }),
+});
--- src/server/routers/spells.ts
+++ src/server/routers/spells.ts
@@ -1,7 +1,45 @@
+import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const spellsRouter = router({
-    get: publicProcedure.query(() => {
-        return ["dupa"];
+    get: publicProcedure.query(async () => {
+        return prisma.spell.findMany();
     }),
+    create: publicProcedure
+        .input(
+            z.object({
+                spellbookId: z.number(),
+                title: z.string(),
+                description: z.string(),
+                image: z.string(),
+            })
+        )
+        .mutation(async (opts) => {
+            const { input } = opts;
+            return prisma.spell.create({
+                data: {
+                    title: input.title,
+                    description: input.description,
+                    image: input.image,
+                    spellbookId: input.spellbookId,
+                },
+            });
+        }),
+    delete: publicProcedure
+        .input(
+            z.object({
+                id: z.number(),
+            })
+        )
+        .mutation(async (opts) => {
+            const { input } = opts;
+            return prisma.spell.delete({
+                where: {
+                    id: input.id,
+                },
+            });
+        }),
 });
