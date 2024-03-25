321b12649a82009b9aef1989a6907b99ceaf506f
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-25 21:08:48 +0100

    test: print out reponse from trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { trpc } from "@/server/client";
+
 export default function Home() {
-    return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+    const spells = trpc.spells.get.useQuery();
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {JSON.stringify(spells.data, null, 2)}
+        </main>
+    );
 }
