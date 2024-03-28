b4dc16de38fae696f3ba5b4b9ec7a07ce9cdbc0e
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-28 10:34:13 +0100

    feat: add speallbook router

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
+import { spellBookRouter } from "./routers/spellbooks";
 import { spellsRouter } from "./routers/spells";
 import { router } from "./trpc";
 
 export const appRouter = router({
     spells: spellsRouter,
+    spellbook: spellBookRouter,
 });
 
 export type AppRouter = typeof appRouter;
