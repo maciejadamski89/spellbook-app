c9bb6485ae87e05423369b8f282b360e59fa38d9
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-28 10:33:51 +0100

    ref: show spellbooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { trpc } from "@/server/client";
+
+import Spellbook from "@/components/spelbook";
 
 export default function Home() {
-    const spells = trpc.spells.get.useQuery();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            {JSON.stringify(spells.data, null, 2)}
+            <Spellbook />
         </main>
     );
 }
