f5f825a7cfb83fcbd7a14cb62410ecafdca462d6
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-04-02 21:20:51 +0200

    style: switch to root layout style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-"use client";
+// "use client";
 
 import Spellbook from "@/components/spelbook";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main>
             <Spellbook />
         </main>
     );
